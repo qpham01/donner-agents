fa2d035190eb7d7561b6d2bcc43d464985e6a69a
quoc <quoc_pham@msn.com>
2025-07-20 20:42:49 -0500

    W4 d3 langgraph with tools and sql memory.

--- 6_mcp/trading_floor.py
+++ 6_mcp/trading_floor.py
@@ -10,11 +10,13 @@ import os
 load_dotenv(override=True)
 
 RUN_EVERY_N_MINUTES = int(os.getenv("RUN_EVERY_N_MINUTES", "60"))
+print(RUN_EVERY_N_MINUTES)
 RUN_EVEN_WHEN_MARKET_IS_CLOSED = (
     os.getenv("RUN_EVEN_WHEN_MARKET_IS_CLOSED", "false").strip().lower() == "true"
 )
+print(RUN_EVEN_WHEN_MARKET_IS_CLOSED)
 USE_MANY_MODELS = os.getenv("USE_MANY_MODELS", "false").strip().lower() == "true"
-
+print(USE_MANY_MODELS)
 names = ["Warren", "George", "Ray", "Cathie"]
 lastnames = ["Patience", "Bold", "Systematic", "Crypto"]
 
@@ -22,10 +24,10 @@ if USE_MANY_MODELS:
     model_names = [
         "gpt-4.1-mini",
         "deepseek-chat",
-        "gemini-2.5-flash-preview-04-17",
-        "grok-3-mini-beta",
+        "gemini-2.5-flash",
+        "grok-3-mini",
     ]
-    short_model_names = ["GPT 4.1 Mini", "DeepSeek V3", "Gemini 2.5 Flash", "Grok 3 Mini"]
+    short_model_names = ["GPT 4.1 Mini", "DeepSeek V3", "Gemini 2.5 Flash", "Gemini 2.5 Flash"]
 else:
     model_names = ["gpt-4o-mini"] * 4
     short_model_names = ["GPT 4o mini"] * 4
